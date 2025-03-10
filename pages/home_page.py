920cb941211c3de2ce4ca85c76b529e4650118b6
Kiran <84580493+kiransrik@users.noreply.github.com>
2025-03-10 12:36:47 +0000

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,13 +3,13 @@ from utils import config
 
 class HomePage:
     def __init__(self, page: Page):
-        self.page = page
+        self.page = page  #Store the Playwright page object
 
     def load(self):
-        self.page.goto(config.BASE_URL)
-        self.page.screenshot(path=f"screenshots/{self.page.title().lower().replace(' ', '_')}.png")
+        self.page.goto(config.BASE_URL) #Open Arqiva website
+        self.page.screenshot(path=f"screenshots/{self.page.title().lower().replace(' ', '_')}.png") #Capture the screenshot of the home page
 
     def verify_homepage_title(self):
-        assert "Arqiva" in self.page.title(), "Homepage title did not match"
+        assert "Arqiva" in self.page.title(), "Homepage title did not match"  #Verify the homepage title
         missing_element_selector = "text=NonExistentElement"
-        assert self.page.locator(missing_element_selector).count() == 0, "Unexpected element found!"
+        assert self.page.locator(missing_element_selector).count() == 0, "Unexpected element found!" #Verify for missing element
